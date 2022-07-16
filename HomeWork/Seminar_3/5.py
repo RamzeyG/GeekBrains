4b04063638cce71f8b8730c1e532a0329a74ef60
Ramzey <remzesii@gmail.com>
2022-07-17 01:49:21 +0500

    исправлено 5 задание

--- HomeWork/Seminar_3/5.py
+++ HomeWork/Seminar_3/5.py
@@ -6,9 +6,21 @@ def func(a):
     array = [0]
     for i in range(0, a):
         array.append(y)
-        array.insert(0, -y)
         y = x + y
         x = y - x
+    
+    
+    count = 1
+    n = -1
+   
+    for i in range(0, a):
+        y = int((-1)**(n+1)*array[count])
+        array.insert(0, y)
+        n = n - 1
+        count = count + 2
+        
+        
     print(array)    
 
 func(8)
+func(12)
